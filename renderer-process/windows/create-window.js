e6bf497f48d44fdcc99d57aa12fe26f4efb28a61
闫文生 <yanwensheng@maoyan.com>
2020-03-23 19:18:08 +0800

    修改提示

--- renderer-process/windows/create-window.js
+++ renderer-process/windows/create-window.js
@@ -2,7 +2,9 @@ const {BrowserWindow} = require('electron').remote
 const path = require('path')
 
 const newWindowBtn = document.getElementById('new-window')
+const fullWinowBtn = document.getElementById('full-window')
 
+//正常窗口
 newWindowBtn.addEventListener('click', (event) => {
   const modalPath = path.join('file://', __dirname, '../../sections/windows/modal.html')
   let win = new BrowserWindow({ width: 400, height: 320 })
@@ -10,4 +12,17 @@ newWindowBtn.addEventListener('click', (event) => {
   win.on('close', () => { win = null })
   win.loadURL(modalPath)
   win.show()
+})
+
+//全屏无边框窗口 
+fullWinowBtn.addEventListener('click', (event) => {
+  const modalPath = path.join('file://', __dirname, '../../sections/windows/modal.html')
+  let win = new BrowserWindow({ width: 400, height: 320,frame:false ,fullscreen:true})
+
+  // win.maximize();
+  win.on('close', () => { win = null })
+  win.loadURL(modalPath)
+  win.on("ready-to-show",()=>{
+    win.show()
+  })
 })
